a4be36e7280778dd4d8addd1662fbc6137aa6886
Abdelrhman Tarek <abdelrhman.tareekk@gmail.com>
2023-08-02 14:31:10 +0300

    Update settings.py

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-h!u8ju3+%_la##%&#lfs22_1%it-q)s$#x@fakf@yel-%&#zev"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
